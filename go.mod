21a321a61dfbcc653dd1266a653b476486cf8119
Rajat Jindal <rajatjindal83@gmail.com>
2020-08-06 19:40:43 +0530

    update

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/rajatjindal/example
 go 1.14
 
 require (
+	github.com/aws/aws-lambda-go v1.19.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.8.0
 	github.com/gorilla/mux v1.7.4
 	github.com/sirupsen/logrus v1.6.0
 )
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/gorilla/mux"
 	"github.com/rajatjindal/example/pkg/api"
 	"github.com/sirupsen/logrus"
 )
+
+var gorillamuxAdapter *gorillamux.GorillaMuxAdapter
 
 func main() {
 	s, err := api.New()
@@ -15,17 +22,32 @@ func main() {
 		logrus.Fatal(err)
 	}
 
+	serveUsingRegular(s.Router)
+}
+
+func serveUsingRegular(r *mux.Router) {
 	server := &http.Server{
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 		IdleTimeout:  120 * time.Second,
 		Addr:         ":8080",
-		Handler:      s.Router,
+		Handler:      r,
 	}
 
 	fmt.Printf("Starting HTTP server on %s\n", server.Addr)
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
 		logrus.Fatalf("server.ListendAndServe() failed with %s", err)
 	}
+}
+
+func serveUsingLambda(r *mux.Router) {
+	gorillamuxAdapter = gorillamux.New(r)
+	lambda.Start(Handler)
+}
+
+//Handler handles the events
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return gorillamuxAdapter.ProxyWithContext(ctx, req)
 }
